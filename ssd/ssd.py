2987ee9e6323469c109b6a4bfe8365e706895920
Daisuke Motoki <daisuke.motoki@pb6a8e15c.kngwnt01.ap.so-net.ne.jp>
2017-07-02 14:14:49 +0900

    make multibox

---
+++ ssd/ssd.py
@@ -0,0 +1,105 @@
+import pickle
+from ssd.models import AVAILABLE_TYPE
+from ssd.models import SSD300
+
+
+class SingleShotMultiBoxDetector:
+    """
+    """
+    ar_presets = dict(
+        ssd300=[[2., 1/2.],
+                [2., 1/2., 3., 1/3.],
+                [2., 1/2., 3., 1/3.],
+                [2., 1/2., 3., 1/3.],
+                [2., 1/2.],
+                [2., 1/2.]]
+        # ssd300=[[2., 1/2.],
+        #         [2., 1/2., 3., 1/3.],
+        #         [2., 1/2., 3., 1/3.],
+        #         [2., 1/2., 3., 1/3.],
+        #         [2., 1/2., 3., 1/3.],
+        #         [2., 1/2., 3., 1/3.]]
+    )
+    scale_presets = dict(
+        ssd300=[(30., 60.),
+                (60., 111.),
+                (111., 162.),
+                (162., 213.),
+                (213., 264.),
+                (264., 315.)]
+        # ssd300=[(30.),
+        #         (60., 111.),
+        #         (111., 162.),
+        #         (162., 213.),
+        #         (213., 264.),
+        #         (264., 315.)]
+    )
+    default_shapes = dict(
+        ssd300=(300, 300, 3)
+    )
+
+    def __init__(self, n_classes, input_shape=None, aspect_ratios=None,
+                 scales=None, variances=None,
+                 model_type="ssd300", base_net="VGG16"):
+        """
+        """
+        self.n_classes = n_classes
+        if input_shape:
+            self.input_shape = input_shape
+        else:
+            self.input_shape = self.default_shapes[model_type]
+        if aspect_ratios:
+            self.aspect_ratios = aspect_ratios
+        else:
+            self.aspect_ratios = self.ar_presets[model_type]
+        if scales:
+            self.scales = scales
+        else:
+            self.scales = self.scale_presets[model_type]
+        if variances:
+            self.variances = variances
+        else:
+            self.variances = [0.1, 0.1, 0.2, 0.2]
+        self.model_type = model_type
+        self.base_net = base_net
+
+        self.model = None
+        self.bboxes = None
+
+    def build(self):
+        """
+        """
+        if self.model_type == "ssd300":
+            self.model, self.bboxes = SSD300(self.input_shape,
+                                             self.n_classes,
+                                             self.base_net,
+                                             self.aspect_ratios,
+                                             self.scales,
+                                             self.variances)
+        else:
+            raise NameError(
+                "{} is not defined. Please select from {}".format(
+                    self.model_type, AVAILABLE_TYPE
+                )
+            )
+
+    def train(self):
+        """
+        """
+        pass
+
+    def save_parameters(self, filepath="./param.pkl"):
+        """
+        """
+        params = dict(
+            n_classes=self.n_classes,
+            input_shape=self.input_shape,
+            model_type=self.model_type,
+            base_net=self.base_net,
+            aspect_ratios=self.aspect_ratios,
+            scales=self.scales,
+            variances=self.variances
+        )
+        print("Writing parameters into {}.".format(filepath))
+        pickle.dump(params, open(filepath, "wb"))
+
--- ssd/utils.py
+++ ssd/utils.py
@@ -1,13 +1,56 @@
+import numpy as np
 
 
-class BoundaryBoxes:
+def make_bboxes(input_shape, feature_map_shape,
+                aspect_ratios, scale):
     """
     """
-    def __init__(self, n_classes=0, prior_boxes=None,
+    map_w = feature_map_shape[1]
+    map_h = feature_map_shape[2]
+    input_w = input_shape[0]
+    input_h = input_shape[1]
+
+    # local box's sizes
+    min_size = scale[0]
+    box_w = [min_size]
+    box_h = [min_size]
+    if len(scale) == 2:
+        box_w.append(np.sqrt(min_size * scale[1]))
+        box_h.append(np.sqrt(min_size * scale[1]))
+    for ar in aspect_ratios:
+        box_w.append(min_size * np.sqrt(ar))
+        box_h.append(min_size / np.sqrt(ar))
+    box_w = np.array(box_w)/2/input_w
+    box_h = np.array(box_h)/2/input_h
+
+    # feature grids
+    step_w = input_w / map_w
+    step_h = input_h / map_h
+    center_w, center_h = np.mgrid[0:map_w, 0:map_h] + 0.5
+    center_w = (center_w * step_w/input_w).reshape(-1, 1)
+    center_h = (center_h * step_h/input_h).reshape(-1, 1)
+
+    n_local_box = len(box_w)
+    bboxes = np.concatenate((center_w, center_h), axis=1)
+    bboxes = np.tile(bboxes, (1, 2 * n_local_box))
+    bboxes[:, ::4] -= box_w
+    bboxes[:, 1::4] -= box_h
+    bboxes[:, 2::4] += box_w
+    bboxes[:, 3::4] += box_h
+    bboxes = bboxes.reshape(-1, 4)
+    bboxes = np.minimum(np.maximum(bboxes, 0.0), 1.0)
+
+    return bboxes
+
+
+class BoundaryBox:
+    """
+    """
+    def __init__(self, n_classes=0, default_boxes=None,
                  overlap_threshold=0.5, nms_threshold=0.45,
                  max_output_size=400):
-        self.n_classes = n_class
-        self.prior_boxes = prior_boxes
+        self.n_classes = n_classes
+        self.default_boxes = default_boxes
         self.overlap_threshold = overlap_threshold
         self.nms_threshold = nms_threshold
 
@@ -17,5 +60,3 @@ class BoundaryBoxes:
     def decode(self):
         pass
 
-    def make_boxes(self):
-        pass
